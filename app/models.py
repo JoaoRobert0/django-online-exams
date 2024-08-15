e7884550d863b4c9d6b001192817258dc746debc
João Roberto <devjoaoroberto@gmail.com>
2024-08-15 09:55:29 -0300

    Creating models, registering in the admin panel and populating the db

--- app/models.py
+++ app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+class Theme(models.Model):
+    title = models.CharField(max_length=255)
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Theme: {self.title}"
+
+class Exam(models.Model):
+    theme = models.ForeignKey(Theme, on_delete=models.CASCADE)
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Exam for Theme: {self.theme.title} (ID: {self.id})"
+
+class Question(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Question: {self.text[:50]}... (ID: {self.id})"
+
+class Choices(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.TextField()
+    is_correct = models.BooleanField(default=False)
+    date_created = models.DateField(auto_now_add=True)
+    date_updated = models.DateField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Choice: {self.text[:50]}... (Correct: {self.is_correct})"
