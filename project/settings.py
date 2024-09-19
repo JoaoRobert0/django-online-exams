ce0b5dd6f42412a01327c059e52440e873030669
JoaoRobert0 <devjoaoroberto@gmail.com>
2024-09-19 18:53:41 +0000

    First setup

--- project/settings.py
+++ project/settings.py
@@ -116,10 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_URL = '/static/'
+
+STATIC_ROOT = '/home/chaves/django-online-exams/staticfiles'  # Onde os arquivos coletados serão armazenados
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
